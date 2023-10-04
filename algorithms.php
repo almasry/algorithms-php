c2a1236a840158a20e359eee2385a164b2368125
Almasry <almasry@area99.com>
2023-10-04 16:44:24 +0300

    Update algorithms.php

--- algorithms.php
+++ algorithms.php
@@ -1,42 +1,41 @@
 <?php
 // Equivalent PHP code for the Encrypt function in the provided Golang code.
 
-function encrypt($plaintext, $key) {
-    // Hash key to ensure it is 256 bits (32 bytes)
+function encrypt($text, $key) {
+    // Hash key to ensure it is 256 bits
     $key = hash('sha256', $key, true);
 
     // Generate a random initialization vector (IV) for AES-256 CBC mode.
-    $iv_size = openssl_cipher_iv_length('aes-256-cbc');
-    $iv = openssl_random_pseudo_bytes($iv_size);
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
 
     // Encrypt the plaintext.
-    $ciphertext = openssl_encrypt($plaintext, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
-
+    $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $text, MCRYPT_MODE_CBC, $iv);
+    
     // Concatenate the IV and the ciphertext and encode them in base64 to create the final encrypted string.
     $encryptedText = base64_encode($iv . $ciphertext);
     
     return $encryptedText;
 }
 
-
 function decrypt($encryptedText, $key) {
-    // Hash key to ensure it is 256 bits (32 bytes)
+    // Hash key to ensure it is 256 bits
     $key = hash('sha256', $key, true);
-
+    
     // Decode the base64 encoded encrypted text
     $encryptedData = base64_decode($encryptedText);
     
     // Extract the IV from the concatenated IV and ciphertext
-    $iv_size = openssl_cipher_iv_length('aes-256-cbc');
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
     $iv = substr($encryptedData, 0, $iv_size);
     
     // Extract the actual ciphertext from the concatenated IV and ciphertext
     $ciphertext = substr($encryptedData, $iv_size);
 
     // Decrypt the ciphertext
-    $plaintext = openssl_decrypt($ciphertext, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+    $plaintext = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext, MCRYPT_MODE_CBC, $iv);
     
-    return $plaintext;
+    return rtrim($plaintext, "\0"); // remove null padding
 }
 
 
